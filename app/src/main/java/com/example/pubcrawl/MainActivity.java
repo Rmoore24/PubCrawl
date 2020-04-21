18ccd57ccd7b8397816b0078eb81ab0144fd6168
Robert Moore <rtm219@humboldt.edu>
2020-04-21 09:05:02 -0700

    Added features for map screen still in dev

--- app/src/main/java/com/example/pubcrawl/MainActivity.java
+++ app/src/main/java/com/example/pubcrawl/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.pubcrawl;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener,ActivityCompat.OnRequestPermissionsResultCallback
 {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
+    private GoogleMap Map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,18 +39,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap)
     {
         //:TODO Fix Map Permissions and implementation
-        if(ActivityCompat.checkSelfPermission(this), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this), Manifest.permission.ACCESS_COARSE_LOCATION)
-                                != PackageManager.PERMISSION_GRANTED)
-        {
-            return;
+        Map = googleMap;
+
+        Map.setOnMyLocationButtonClickListener(this);
+        Map.setOnMyLocationClickListener(this);
+        enableMyLocation();
+    }
+
+    private void enableMyLocation()
+    {
+        // [START maps_check_location_permission]
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (Map != null) {
+                Map.setMyLocationEnabled(true);
+            }
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
         }
-        googleMap.setMyLocationEnabled(true);
-        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        //else
-       // {
-        //    Toast.makeText(this, "Permission Error", Toast.LENGTH_LONG).show();
-        //}
+        // [END maps_check_location_permission]
+    }
+
+
+    @Override
+    public boolean onMyLocationButtonClick()
+    {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location)
+    {
+
     }
 }
